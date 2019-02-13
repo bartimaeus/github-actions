ba7d4d5870ff7b7406eb3d0f195112dbe6d310cb
Eric Shelley <eric@webdesignbakery.com>
2019-02-13 14:42:48 -0700

    Download and install ecs-deploy in the Dockerfile rather than entrypoint.sh

--- ecs/entrypoint.sh
+++ ecs/entrypoint.sh
@@ -15,11 +15,6 @@ errorNotification() {
   slack-notify "$SLACK_MESSAGE", "$SLACK_COLOR"
 }
 
-# Download the latest version of ecs-deploy
-curl -o /usr/local/bin/ecs-deploy https://raw.githubusercontent.com/silinternational/ecs-deploy/develop/ecs-deploy
-chmod +x /usr/local/bin/ecs-deploy
-echo "Successfully downloaded ecs-deploy"
-
 # If PROD_ environment variables provided, create aws credentials file
 if [ -n "${PROD_AWS_ACCESS_KEY_ID}" ] && [ -n "${PROD_AWS_SECRET_ACCESS_KEY}" ]; then
     mkdir -p $HOME/.aws
