950363dcfe79057d9823457b18a28f6a5f9d01b3
Eric Shelley <eric@webdesignbakery.com>
2019-02-13 11:16:30 -0700

    Removing -o pipefail from slack/setup.sh

--- slack/setup.sh
+++ slack/setup.sh
@@ -3,7 +3,6 @@
 { # this ensures the entire script is downloaded #
 
 set -e
-set -o pipefail
 
 echo -e "\033[36m~> \033[34mInstalling slack-notify...\033[39m"
 curl -o /usr/local/bin/slack-notify https://bartimae.us/github-actions/slack/notify.sh
