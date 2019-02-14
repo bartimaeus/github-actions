0292af21a79f56e9920d11fac709e0ef57ee2f30
Eric Shelley <eric@webdesignbakery.com>
2019-02-13 22:06:06 -0700

    Allow for a successful slack webhook url

--- slack/entrypoint.sh
+++ slack/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Allow a successful slack webhook url to be used in place of the default webhook
+if [ -n "${SUCCESS_SLACK_WEBHOOK_URL}" ]; then
+  export SLACK_WEBHOOK_URL=$SUCCESS_SLACK_WEBHOOK_URL
+fi
+
 slack-notify "$SLACK_MESSAGE" "$SLACK_COLOR"
 
 echo "Notification successfully sent to slack."
